59598f81fd03257a5df73a43b6340e2b0d1bcedd
Quoc An <nqan69@gmail.com>
2023-05-20 17:28:58 +0700

    done navbar

--- src/components/Layout/components/Navigationbar/Menu/MenuItem.jsx
+++ src/components/Layout/components/Navigationbar/Menu/MenuItem.jsx
@@ -1,10 +1,15 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
 function MenuItem({ title, to, icon }) {
   return (
     <NavLink
-      className="max-w-[76px] p-3 mt-4 ml-2 flex-col items-center flex text-xs rounded-3xl hover:bg-themecolor-100"
+      className="max-w-[76px] p-3 mt-4 ml-2 flex-col items-center flex text-xs
+    rounded-3xl transition-color duration-200 hover:bg-themecolor-100"
+      style={({ isActive }) => ({
+        backgroundColor: isActive ? '#C0FFF4' : '',
+      })}
       to={to}
     >
       {icon}
